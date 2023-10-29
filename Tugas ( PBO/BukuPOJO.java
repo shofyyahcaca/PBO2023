1455a6c737f0026f28f3d7af8baa0b97f16d4a0b
shofyyahcaca <148856320+shofyyahcaca@users.noreply.github.com>
2023-10-29 22:07:02 +0700

    Add files via upload

---
+++ Tugas ( PBO/BukuPOJO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package buku;
+
+/**
+ *
+ * @author SHOFYYAH
+ */
+public class BukuPOJO {
+
+    private String ISBN;
+    private String Judul_Buku;
+    private String Tahun_Terbit;
+    private String Penerbit;
+
+    /**
+     * @return the ISBN
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the Judul_Buku
+     */
+    public String getJudul_Buku() {
+        return Judul_Buku;
+    }
+
+    /**
+     * @param Judul_Buku the Judul_Buku to set
+     */
+    public void setJudul_Buku(String Judul_Buku) {
+        this.Judul_Buku = Judul_Buku;
+    }
+
+    /**
+     * @return the Tahun_Terbit
+     */
+    public String getTahun_Terbit() {
+        return Tahun_Terbit;
+    }
+
+    /**
+     * @param Tahun_Terbit the Tahun_Terbit to set
+     */
+    public void setTahun_Terbit(String Tahun_Terbit) {
+        this.Tahun_Terbit = Tahun_Terbit;
+    }
+
+    /**
+     * @return the Penerbit
+     */
+    public String getPenerbit() {
+        return Penerbit;
+    }
+
+    /**
+     * @param Penerbit the Penerbit to set
+     */
+    public void setPenerbit(String Penerbit) {
+        this.Penerbit = Penerbit;
+    }
+
+    
+}
