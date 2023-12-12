481a22131ba21e66d144f0e4d61b721c37cfe8d7
shofyyahcaca <148856320+shofyyahcaca@users.noreply.github.com>
2023-12-13 03:25:48 +0700

    Add files via upload

---
+++ Final Project/Persistence/DataBuku.java
@@ -0,0 +1,383 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perpustakaan;
+
+/**
+ *
+ * @author SHOFYYAH
+ */
+public class DataBuku {
+
+    /**
+     * @param args the command line arguments
+     */
+    private String Username;
+    private String Password;
+    
+    private String ISBN;
+    private String Judul_Buku;
+    private String Pengarang;
+    private String Penerbit;
+    private String Tahun_Terbit;
+    private String idSkripsi;
+    private String judulSkripsi;
+    private String Fakultas;
+    private String prodi;
+    private String Pembimbing;
+    private String Tahun;
+    private String Penulis;
+    private String jumlahHalaman;
+    private String IdSkripsi;
+    private String JudulSkripsi;
+    private String IdPeminjaman;
+    private String NamaPeminjam;
+    private String Nim;
+    private String Semester;
+    private String Prodi;
+    private String TanggalPeminjaman;
+    private String TanggalPengembalian;
+    private String Status;
+    private String Kategori;
+    private String KategoriSkripsi;
+            
+
+    /**
+     * @return the Username
+     */
+    public String getUsername() {
+        return Username;
+    }
+
+    /**
+     * @param Username the Username to set
+     */
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    /**
+     * @return the Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password the Password to set
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+
+    /**
+     * @return the ISBN
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the Judul_Buku
+     */
+    public String getJudul_Buku() {
+        return Judul_Buku;
+    }
+
+    /**
+     * @param Judul_Buku the Judul_Buku to set
+     */
+    public void setJudul_Buku(String Judul_Buku) {
+        this.Judul_Buku = Judul_Buku;
+    }
+
+    /**
+     * @return the Pengarang
+     */
+    public String getPengarang() {
+        return Pengarang;
+    }
+
+    /**
+     * @param Pengarang the Pengarang to set
+     */
+    public void setPengarang(String Pengarang) {
+        this.Pengarang = Pengarang;
+    }
+
+    /**
+     * @return the Penerbit
+     */
+    public String getPenerbit() {
+        return Penerbit;
+    }
+
+    /**
+     * @param Penerbit the Penerbit to set
+     */
+    public void setPenerbit(String Penerbit) {
+        this.Penerbit = Penerbit;
+    }
+
+    /**
+     * @return the Tahun_Terbit
+     */
+    public String getTahun_Terbit() {
+        return Tahun_Terbit;
+    }
+
+    /**
+     * @param Tahun_Terbit the Tahun_Terbit to set
+     */
+    public void setTahun_Terbit(String Tahun_Terbit) {
+        this.Tahun_Terbit = Tahun_Terbit;
+    }
+
+    /**
+     * @return the idSkripsi
+     */
+    public String getIdSkripsi() {
+        return idSkripsi;
+    }
+
+    /**
+     * @param idSkripsi the idSkripsi to set
+     */
+    public void setIdSkripsi(String idSkripsi) {
+        this.idSkripsi = idSkripsi;
+    }
+
+    /**
+     * @return the judulSkripsi
+     */
+    public String getJudulSkripsi() {
+        return judulSkripsi;
+    }
+
+    /**
+     * @param judulSkripsi the judulSkripsi to set
+     */
+    public void setJudulSkripsi(String judulSkripsi) {
+        this.judulSkripsi = judulSkripsi;
+    }
+
+    /**
+     * @return the Fakultas
+     */
+    public String getFakultas() {
+        return Fakultas;
+    }
+
+    /**
+     * @param Fakultas the Fakultas to set
+     */
+    public void setFakultas(String Fakultas) {
+        this.Fakultas = Fakultas;
+    }
+
+    /**
+     * @return the prodi
+     */
+    public String getProdi() {
+        return prodi;
+    }
+
+    /**
+     * @param prodi the prodi to set
+     */
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+
+    /**
+     * @return the Pembimbing
+     */
+    public String getPembimbing() {
+        return Pembimbing;
+    }
+
+    /**
+     * @param Pembimbing the Pembimbing to set
+     */
+    public void setPembimbing(String Pembimbing) {
+        this.Pembimbing = Pembimbing;
+    }
+
+    /**
+     * @return the Tahun
+     */
+    public String getTahun() {
+        return Tahun;
+    }
+
+    /**
+     * @param Tahun the Tahun to set
+     */
+    public void setTahun(String Tahun) {
+        this.Tahun = Tahun;
+    }
+
+    /**
+     * @return the Penulis
+     */
+    public String getPenulis() {
+        return Penulis;
+    }
+
+    /**
+     * @param Penulis the Penulis to set
+     */
+    public void setPenulis(String Penulis) {
+        this.Penulis = Penulis;
+    }
+
+    /**
+     * @return the jumlahHalaman
+     */
+    public String getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+
+    /**
+     * @param jumlahHalaman the jumlahHalaman to set
+     */
+    public void setJumlahHalaman(String jumlahHalaman) {
+        this.jumlahHalaman = jumlahHalaman;
+    }
+
+    /**
+     * @return the IdPeminjaman
+     */
+    public String getIdPeminjaman() {
+        return IdPeminjaman;
+    }
+
+    /**
+     * @param IdPeminjaman the IdPeminjaman to set
+     */
+    public void setIdPeminjaman(String IdPeminjaman) {
+        this.IdPeminjaman = IdPeminjaman;
+    }
+
+    /**
+     * @return the NamaPeminjam
+     */
+    public String getNamaPeminjam() {
+        return NamaPeminjam;
+    }
+
+    /**
+     * @param NamaPeminjam the NamaPeminjam to set
+     */
+    public void setNamaPeminjam(String NamaPeminjam) {
+        this.NamaPeminjam = NamaPeminjam;
+    }
+
+    /**
+     * @return the Nim
+     */
+    public String getNim() {
+        return Nim;
+    }
+
+    /**
+     * @param Nim the Nim to set
+     */
+    public void setNim(String Nim) {
+        this.Nim = Nim;
+    }
+
+    /**
+     * @return the Semester
+     */
+    public String getSemester() {
+        return Semester;
+    }
+
+    /**
+     * @param Semester the Semester to set
+     */
+    public void setSemester(String Semester) {
+        this.Semester = Semester;
+    }
+
+    /**
+     * @return the TanggalPeminjaman
+     */
+    public String getTanggalPeminjaman() {
+        return TanggalPeminjaman;
+    }
+
+    /**
+     * @param TanggalPeminjaman the TanggalPeminjaman to set
+     */
+    public void setTanggalPeminjaman(String TanggalPeminjaman) {
+        this.TanggalPeminjaman = TanggalPeminjaman;
+    }
+
+    /**
+     * @return the TanggalPengembalian
+     */
+    public String getTanggalPengembalian() {
+        return TanggalPengembalian;
+    }
+
+    /**
+     * @param TanggalPengembalian the TanggalPengembalian to set
+     */
+    public void setTanggalPengembalian(String TanggalPengembalian) {
+        this.TanggalPengembalian = TanggalPengembalian;
+    }
+
+    /**
+     * @return the Status
+     */
+    public String getStatus() {
+        return Status;
+    }
+
+    /**
+     * @param Status the Status to set
+     */
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    /**
+     * @return the Kategori
+     */
+    public String getKategori() {
+        return Kategori;
+    }
+
+    /**
+     * @param Kategori the Kategori to set
+     */
+    public void setKategori(String Kategori) {
+        this.Kategori = Kategori;
+    }
+
+    /**
+     * @return the KategoriSkripsi
+     */
+    public String getKategoriSkripsi() {
+        return KategoriSkripsi;
+    }
+
+    /**
+     * @param KategoriSkripsi the KategoriSkripsi to set
+     */
+    public void setKategoriSkripsi(String KategoriSkripsi) {
+        this.KategoriSkripsi = KategoriSkripsi;
+    }
+    
+}
